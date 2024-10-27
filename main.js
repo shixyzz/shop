efaf394d20b08dbf13fee5faab966218d2eca349
shixyzz <danyareva965@gmail.com>
2024-10-27 14:51:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+document.getElementById("productsButton").addEventListener("click", function() {
+    window.location.href = "https://my-project-dun-phi.vercel.app";
+});
 
+document.getElementById("newsButton").addEventListener("click", function() {
+    window.location.href = "https://news-nine-omega.vercel.app";
+});
+
+document.getElementById("categoryButton").addEventListener("click", function() {
+    window.location.href = "https://category-iota.vercel.app";
+});
